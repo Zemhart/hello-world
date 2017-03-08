84b92735cec90d1f6a4a3214f6caa18ea92671de
jimmymsz <jimmymsz@ymail.com>
2017-03-08 12:23:55 +0700

    hello world main program in cpp

---
+++ hello-world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a = 0;
+	a += 1;
+	a *=100;
+	a += (2*10);
+	a += 3;
+	
+	cout<<"Hello "<<a<<" !"<<endl;
+	
+	cout<<"for testing and development only"<<endl;
+	cout<<"copyright (r) 2017"<<endl;
+	
+	return 0;
+}
